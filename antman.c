2373b4072e28c9a6818ec8d0dc559286ceae4c9c
Ramirez-Nicolas <98922442+Ramirez-Nicolas@users.noreply.github.com>
2022-03-16 02:04:20 +0100

    Initial commit

---
+++ antman.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** cprsd_data.c
+** File description:
+** printf compressed data
+*/
+
+#include <stdarg.h>
+#include "./includes/bsprintf.h"
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void)
+{
+    my_printf("%s", "COMPRESSED DATA\n");
+    return (0);
+}
---
+++ giantman.c
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2021
+** origin_uncomp.c
+** File description:
+** oui
+*/
+
+#include <stdarg.h>
+#include "./includes/bsprintf.h"
+#include <unistd.h>
+#include <stdio.h>
+
+int main(void)
+{
+    my_printf("%s", "ORIGINAL UNCOMPRESSED FILE\n");
+    return (0);
+}
